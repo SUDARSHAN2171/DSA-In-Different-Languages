8cdab7e70b3b2d0160a3de00ed93c7163b72fd6a
SUDARSHAN9171 <145355498+SUDARSHAN9171@users.noreply.github.com>
2024-05-11 23:17:45 +0530

    Added about

---
+++ C Codes/SelectionSort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int SelectionSort(int array[],int size)
+{
+    for(int i = 0 ; i < size ; i++ )
+    {
+        for(int j=i+1;j < size ; j++)
+        {
+            if(array[j]<array[i])
+            {
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    printf("Sorted Array : ");
+    for(int i = 0 ; i < size ; i++)
+    {
+        printf("%d ",array[i]);
+    }
+}
+int main()
+{
+    int size;
+    printf("Enter the size of array : ");
+    scanf("%d",&size);
+    int array[size];
+    printf("Enter the element in array : ");
+    for(int i = 0 ; i < size ; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    SelectionSort(array,size);
+}
