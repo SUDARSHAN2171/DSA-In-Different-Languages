ca869968bb33aa72e1d43d249a1e18379cbf6fe5
SUDARSHAN9171 <145355498+SUDARSHAN9171@users.noreply.github.com>
2024-05-18 13:50:24 +0530

    Added about

--- C Codes/SelectionSort.c
+++ C Codes/SelectionSort.c
@@ -3,14 +3,16 @@ int SelectionSort(int array[],int size)
 {
     for(int i = 0 ; i < size -1 ; i++ )
     {
-        for(int j=i+1;j < size ; j++)
+       
+        int min = i;
+        for(int j = i+1 ; j <size ; j++ )
+        if(array[j]<array[min])
+        {  
+            min = j;
+        }
+        if(min != i)
         {
-            if(array[j]<array[i])
-            {
-                int temp = array[j];
-                array[j] = array[i];
-                array[i] = temp;
-            }
+            swap(array[i],array[min]);
         }
     }
     printf("Sorted Array : ");
--- CPP Codes/SelectionSort.cpp
+++ CPP Codes/SelectionSort.cpp
@@ -4,12 +4,15 @@ int SelectionSort(int array[],int size)
 {
     for(int i = 0 ;i < size -1 ; i++ )
     {
+        int min = i;
         for(int j = i+1 ; j <size ; j++ )
-        if(array[j]<array[i])
+        if(array[j]<array[min])
+        {  
+            min = j;
+        }
+        if(min != i)
         {
-            int temp = array[j];
-            array[j] = array[i];
-            array[i] = temp;
+            swap(array[i],array[min]);
         }
     }
     cout<<endl<<"Sorted Array : ";
